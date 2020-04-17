ed16404054c7618a7c2541adc94815f6fb4590c7
jopalhei <45241927+jopalhei@users.noreply.github.com>
2020-04-17 16:26:38 +0100

    Add files via upload

---
+++ Stop-TraceWithPerf.ps1
@@ -0,0 +1,55 @@
+<#
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+param(
+    $maxlatencyms = 10,    
+    $TimeToEnd = 10
+    
+)
+$maxlatency = $maxlatencyms/1000
+$paths = (Get-Counter -ListSet Physicaldisk).PathsWithInstances | where {$_ -like "*\Avg. Disk sec/Transfer" -and $_ -notlike "\PhysicalDisk(_Total)\*" }
+logman delete PerfLog-Short | Out-Null
+Logman.exe create counter PerfLog-Short -o C:\Temp\PerfLog-Short.blg -f bincirc -v mmddhhmm -max 500 -c "\LogicalDisk(*)\*" "\Memory\*" "\Cache\*" "\Network Interface(*)\*{" "\Netlogon(*)\*" "\Paging File(*)\*" "\PhysicalDisk(*)\*" "\Processor(*)\*" "\Processor Information(*)\*" "\Process(*)\*" "\Thread(*)\*" "\Redirector\*" "\Server\*" "\System\*" "\Server Work Queues(*)\*" "\Terminal Services\*" -si 00:00:01
+logman start PerfLog-Short
+logman create trace "storport" -ow -o c:\temp\storport.etl -p "Microsoft-Windows-StorPort" 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 4096 -ets
+$run = $true
+do {
+        $values = Get-Counter -Counter $paths
+
+        foreach ($value in $values.CounterSamples)
+        {
+            if ($value.Cookedvalue -ge $maxlatency)
+            {
+                $Message = "The Stop was triggered at " + (Get-Date) + " on the following counter "
+                $Message | Out-File "C:\temp\PerfCounterTrigger.txt"
+                $value | Out-File "C:\temp\PerfCounterTrigger.txt" -Append
+                Start-Sleep -Seconds $TimeToEnd
+                logman stop PerfLog-Short
+                logman stop "Storport" -ets
+                $run = $false
+                Break
+            }
+        }
+    Start-Sleep -Seconds 1
+} while ($run)
+logman delete PerfLog-Short
+
